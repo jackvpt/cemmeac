92ee9399518d0ebb7c5d3a1fa8f41e2194cf2b56
Jacques Verpoest <jacques.verpoest@gmail.com>
2025-12-15 23:23:14 +0100

    prices

--- frontend/src/data/prices.js
+++ frontend/src/data/prices.js
@@ -4,7 +4,7 @@ export const prices = [
     duration: 45,
     price: 110,
     description:
-      "Tests ophtalmologiques, audiométrie, bandelette urinaire, ECG et examen clinique, dépistage risque cardio-vasculaires à partir de 40 ans.",
+      "Tests ophtalmologiques, audiométrie, bandelette urinaire, ECG et examen clinique, dépistage risque cardio-vasculaires à partir de 40 ans",
   },
   {
     name: "VISITE CLASSE DEUX PLUS DE 60 ANS",
@@ -21,12 +21,20 @@ export const prices = [
       "Tests ophtalmologiques complets, audiométrie, bandelette urinaire, ECG et examen clinique, dépistage risque cardio-vasculaires à partir de 40 ans.",
   },
   {
+    name: "VISITE PRÉ-ADMISSION CLASSE UNE",
+    duration: 60,
+    price: 135,
+    description:
+      "Tests ophtalmologiques complets, audiométrie, bandelette urinaire, HémoCue, ECG et examen clinique, dépistage risque cardio-vasculaires approfondis à partir de 40 ans.",
+  },
+  {
     name: "VISITE CLASSE UNE",
     duration: 90,
     price: 270,
     description:
       "Tests ophtalmologiques complets, audiométrie, bandelette urinaire, HémoCue, ECG et examen clinique, dépistage risque cardio-vasculaires approfondis à partir de 40 ans, test cognitif et dépistage syndrome d’apnée du sommeil en fonction âge.",
   },
+
   {
     name: "CONSULTATION SIMPLE",
     duration: 15,
@@ -38,18 +46,6 @@ export const prices = [
     duration: 30,
     price: 60,
     description: "",
-  },
-  {
-    name: "COTATION ECG",
-    duration: null,
-    price: 14.26,
-    description: "Tarif conventionné sécurité sociale DEQP003.",
-  },
-  {
-    name: "CONSULTATION MICRONUTRITION",
-    duration: 30,
-    price: 60,
-    description: "Diète protéinée.",
   },
   {
     name: "PRÉPARATION DOSSIER EN RENVOI OU CONCERTATION (DÉROGATION DGAC)",
--- frontend/src/pages/Activities/Activities.jsx
+++ frontend/src/pages/Activities/Activities.jsx
@@ -12,6 +12,33 @@ const Activities = () => {
       <Header />
       <div className="activities__body">
         <h1>Activités</h1>
+        <p className="activities__body-first-paragraph">
+          Bienvenue au Cabinet d’expertise médicale en médecine aeronautique de
+          Cornouaille au profit des PNT (renouvellement aptitude classe UNE),
+          PNC (admission et renouvellement) et pilote privé (admission et
+          renouvellement aptitude classe DEUX).
+        </p>
+        <p className="activities__body-second-paragraph">
+          Vous rêvez d’une carrière professionnelle militaire ou civile et vous
+          avez des doutes sur votre aptitude ? Nous vous proposons la
+          réalisation d’une visite médicale identique à celle du renouvellement
+          d’une aptitude classe UNE avant de vous lancer dans votre projet.
+        </p>
+        <p className="activities__body-third-paragraph">
+          Idéalement situé à 10 min en voiture au Nord de Lorient ; à 5 min à
+          pied de la gare de Quimperlé, ou à 2 min de la voie express N165, le
+          cabinet est facile d’accès et propose un grand parking devant le
+          cabinet.
+        </p>
+        <p>
+          Nous nous ferons un plaisir de vous recevoir afin de vous proposer un
+          service de qualité. Nous réalisons tous les examens nécessaires à la
+          réalisation de votre visite d’aptitude, avec un bilan ophtalmologique
+          exhaustif réalisé avec du matériel ultraperformant, une audiométrie en
+          cabine, un tracé ECG et nous disposons de tout le matériel médical
+          nécessaire pour valider votre certificat médical.
+        </p>
+        <p>Au plaisir de vous recevoir</p>
       </div>
     </section>
   )
